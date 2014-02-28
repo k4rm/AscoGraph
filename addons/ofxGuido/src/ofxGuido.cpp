872b04396a1b18ebc1cb6984c15c082f4ad98ea4
Thomas Coffy <thomas.coffy@ircam.fr>
2014-02-28 12:34:29 +0100

    ofxGuido

---
+++ addons/ofxGuido/src/ofxGuido.cpp
@@ -0,0 +1,38 @@
+#include "ofxGuido.h"
+
+
+ofxGuido::ofxGuido(GuidoLayoutSettings& layoutSettings)
+{
+	guido = new GuidoComponent();
+	guido->GuidoInit("DroidSansMono.ttf", "GUI/guido2.ttf");
+	guido->setGuidoLayoutSettings(layoutSettings);
+}
+
+
+bool ofxGuido::compile_string(const string& gstr)
+{
+	int err = guido->setGMNCode(gstr.c_str());
+	return !(err == -1);
+}
+
+void ofxGuido::getPageFormat(GuidoPageFormat& format)
+{
+	GuidoGetPageFormat(guido->getGRHandler(), 1, &format);
+}
+
+void ofxGuido::draw_cache(int x, int y) {
+	if (guido)
+		guido->getDevice()->drawCache.draw(x, y);
+}
+
+void ofxGuido::draw(int x, int y, int w, int h) {
+	if (guido)
+		guido->draw(x, y, w, h);
+}
+
+void ofxGuido::setSize(int w, int h) {
+	if (guido) {
+		guido->setSize(w, h);
+		guido->getDevice()->NotifySize(w, h);
+	}
+}
---
+++ addons/ofxGuido/src/ofxGuido.h
@@ -0,0 +1,26 @@
+#include "GuidoComponent.h"
+#include "openFrameworksDevice.h"
+
+/*
+ * Simple wrapper for Guido engine library
+ * http://guidolib.sourceforge.net/
+ */
+
+class ofxGuido
+{
+public:
+	ofxGuido(GuidoLayoutSettings& layoutSettings);
+	~ofxGuido() {}
+
+	bool compile_string(const string& gstr);
+	void getPageFormat(GuidoPageFormat& format);
+	void draw_cache(int x, int y);
+	void draw(int x, int y, int w, int h);
+
+	void setWidth(int w) { if (guido) guido->setWidth(w); }
+	void setHeight(int h) { if (guido) guido->setHeight(h); }
+	int getWidth() { if (guido) return guido->getWidth(); else return 0; }
+	int getHeight() { if (guido) return guido->getHeight(); else return 0; }
+	void setSize(int w, int h);
+	GuidoComponent* guido;
+};
